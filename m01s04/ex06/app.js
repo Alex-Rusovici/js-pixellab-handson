295fae609fb54853bfa3361a1843f374c19891bc
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-12-01 22:00:11 +0200

    01.12 - classwork
    01.12 - classwork

---
+++ m01s04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Alexandru',
+  surname: 'Rusovici',
+  age: 30,
+  petOwner: true,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steve',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
