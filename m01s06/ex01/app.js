7f5dc7751d41c9bbbaa91fb6aacdf7da8b7c4057
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-12-20 21:58:38 +0200

    20.12 - classwork
    20.12 - classwork

---
+++ m01s06/ex01/app.js
@@ -0,0 +1,76 @@
+/* Adauga o proprietate booleana numita isTrunkOpen si asigneaza-i valoarea false.
+ * Adauga metode numite openTrunk() si closeTrunk() care sa seteze proprietatea isTrunkOpen pe true, respectiv false.
+ * Adauga o proprietate booleana numita areLightsOn si asigneaza-i valoarea false.
+ * Adauga metode numite turnLightsOn() si turnLightsOff() care sa manipuleze valoarea.
+ * Adauga o metoda numita flashLights() care sa apeleze cele doua metode in succesiune, folosind metoda window.setTimeout().
+ */
+
+const car = {
+  make: 'Audi',
+  color: 'black',
+  wheels: 4,
+  speed: 50,
+  topSpeed: 160,
+  topReverseSpeed: -50,
+  isTrunkOpen: false,
+  areLightsOn: false,
+  accelerate: function () {
+    this.speed++;
+  },
+  decelerate: function () {
+    this.speed--;
+  },
+  openTrunk: function () {
+    this.isTrunkOpen = true;
+  },
+  closeTrunk: function () {
+    this.isTrunkOpen = false;
+  },
+  turnLightsOn: function () {
+    this.areLightsOn = true;
+  },
+  turnLightsOff: function () {
+    this.areLightsOn = false;
+  },
+  flashLights: function () {
+    this.turnLightsOn();
+    // this solution - v1
+    const self = this;
+
+    setTimeout(function (self) {
+      self.turnLightsOff();
+    }, 6000);
+  },
+  stop: function () {
+    this.speed = 0;
+  },
+  setSpeed: function (input) {
+    if (input <= this.topSpeed && input >= this.topReverseSpeed) {
+      this.speed = input;
+    } else {
+      console.warn(`car can't vroom vroom that fast`);
+    }
+  },
+};
+
+console.warn(
+  `Afiseaza propozitia: "Masina era marca make si se deplasa cu speed km/h.".`,
+);
+
+console.log(
+  `Masina este marca ${car.make} si se deplasa cu ${car.speed} km/h.`,
+);
+
+console.warn(`
+Decelereaza masina cu 5 unitati apoi afisaza propozitia: "Viteza noua este speed km/h".
+`);
+
+car.decelerate();
+car.decelerate();
+car.decelerate();
+car.decelerate();
+car.decelerate();
+
+console.log(`Viteza noua este ${car.speed} km/h.`);
+
+car.setSpeed(170);
