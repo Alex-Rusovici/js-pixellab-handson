f70c6049a7b8de814866e7b3a95bf7b74f7a4c63
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-01-17 21:52:55 +0200

    17-01 - classwork
    17-01 - classwork

--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -5,7 +5,7 @@
     stage.addEventListener('mouseover', () => {
       const message = 'Mouseul este pe scena';
       console.log(message);
-      showMessage(message);
+      showMessage(message, 'message');
 
       document.body.append(paragraphElement);
     });
@@ -14,14 +14,14 @@
       const message = 'Mouseul nu este pe scena';
       console.log('Mouseul nu este pe scena');
       console.log(message);
-      showMessage(message);
+      showMessage(message, 'message');
     });
 
     // function functions are hoisted
-    function showMessage(message) {
+    function showMessage(message, cssClass = '') {
       const paragraphElement = document.createElement('p');
       paragraphElement.innerText = message;
-      paragraphElement.classList.add('message');
+      paragraphElement.classList.add(cssClass);
 
       document.body.append(paragraphElement);
     }
