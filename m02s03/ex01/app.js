1516a3815021b7ab43a9e70cc1c0a40b9a728d8f
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-02-02 21:53:05 +0200

    jQuery app finished
    jQuery app finished

---
+++ m02s03/ex01/app.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor(make, color, wheels, speed) {
+    this.make = make;
+    this.color = color;
+    this.wheels = wheels;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed = this.speed + 1;
+  }
+
+  decelerate() {
+    this.speed = this.speed - 1;
+  }
+}
+
+const audi = new Car('Audi', 'black', 4, 50);
